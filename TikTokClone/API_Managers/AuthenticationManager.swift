8251c83e3a6fd144364235790cec37ade2beb11f
ToshiroSama <alishdjuraev@gmail.com>
2022-06-14 16:35:01 +0500

    I've created bunch of files. Using MVVM & MVC patterns. Pod was installed.

---
+++ TikTokClone/API_Managers/AuthenticationManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationManager.swift
+//  TikTokClone
+//
+//  Created by Alisher Djuraev on 14/06/22.
+//
+
+import Foundation
---
+++ TikTokClone/API_Managers/HapticsManager.swift
@@ -0,0 +1,8 @@
+//
+//  HapticsManager.swift
+//  TikTokClone
+//
+//  Created by Alisher Djuraev on 14/06/22.
+//
+
+import Foundation
