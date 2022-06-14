20985c3a29979f60726c39405fdb71fcaa272fdd
ToshiroSama <alishdjuraev@gmail.com>
2022-06-14 15:58:17 +0500

    Initial Commit

---
+++ TikTokClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTokClone
+//
+//  Created by Alisher Djuraev on 14/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
