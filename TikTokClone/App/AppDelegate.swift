5404ec6142190f93cb32242d7a63a7da0df848af
ToshiroSama <alishdjuraev@gmail.com>
2022-06-15 10:29:27 +0500

    TabBarController is set up and activated with the necessary titles and icons for our navigation controllers

--- TikTokClone/App/AppDelegate.swift
+++ TikTokClone/App/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Alisher Djuraev on 14/06/22.
 //
 
+import Firebase
 import UIKit
 
 @main
@@ -14,9 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = HomeViewController()
+        window.rootViewController = TabBarViewController()
         self.window = window
         self.window?.makeKeyAndVisible()
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
